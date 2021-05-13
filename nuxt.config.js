678d4617940b799a5cb7098f0da2aec868489efb
Konstantin BIFERT <hello@kissu.io>
2021-05-13 14:24:57 +0200

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,50 @@
+export default {
+  /*
+   ** Headers of the page
+   ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
+   */
+  head: {
+    title: "Nuxt.js starter for CSB",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Official Nuxt.js starter for CodeSandBox"
+      }
+    ],
+    script: [
+      {
+        src:
+          "https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js"
+      }
+    ],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css"
+      }
+    ]
+  },
+
+  /*
+   ** Nuxt.js modules
+   ** Doc: https://modules.nuxtjs.org
+   */
+  modules: [],
+
+  /*
+   ** Global CSS
+   ** Doc: https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-css
+   */
+  css: [],
+
+  /*
+   ** Plugins to load before mounting the App
+   ** Doc: https://nuxtjs.org/docs/2.x/directory-structure/plugins
+   */
+  plugins: []
+};
